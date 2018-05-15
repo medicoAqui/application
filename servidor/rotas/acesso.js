67370318e4ce79c7cfb3ebf5e22daabb75b9bd16
bernardlcs <bernard.lenin4@gmail.com>
2018-05-15 12:20:03 -0300

    commti de acesso

--- servidor/rotas/acesso.js
+++ servidor/rotas/acesso.js
@@ -3,64 +3,49 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var UserCliente = require('../modelos/userCliente.js');
 var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+var app = express();
 var config = require('../config');
-var checarToken = require('./checarToken');
 
 
+router.post('/authenticate', function(req, res) {
 
-router.post('/register', function(req, res) {
+    UserCliente.findOne({cpf: req.body.cpf}, function (err, user) {
+        console.log(req.body.cpf );
+        console.log(user);
 
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+        if (err) {
+            throw err;
+        }
+
+        if (!user) {
+            res.json({success: false, message: 'Usuario não encontrado'});
+        } else if (user) {
 
 
-    UserCliente.create((req.body),
+            if (user.password != req.body.password) {
+                res.json({success: false, message: 'Senha Invalida'});
+            } else {
 
-        function (err, user) {
-            console.log(user.nome);
-            console.log(user.cpf);
-            console.log(user.password);
-            if (err) return res.status(500).send("There was a problem registering the user.")
-            // create a token
-            var token = jwt.sign({ id: user._id }, config.secret, {
-                expiresIn: 86400 // expires in 24 hours
-            });
-            res.status(200).send({ auth: true, token: token });
-        });
-});
+                // cria o token
+                var token = jwt.sign(user, 'schotpilgrim', {
+                    expiresIn: 86400 // expires in 24 hours
+                });
 
-router.post('/login', function(req, res) {
-    UserCliente.findOne({ email: req.body.email }, function (err, user) {
-        if (err) return res.status(500).send('Error on the server.');
-        if (!user) return res.status(404).send('No user found.');
-
-        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
-        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-        var token = jwt.sign({ id: user._id }, config.secret, {
-            expiresIn: 86400 // expires in 24 hours
-        });
-        res.status(200).send({ auth: true, token: token });
+                res.json({
+                    usuario: user,
+                    success: true,
+                    message: 'Aqui o token!',
+                    token: token
+                });
+            }
+        }
     });
 });
 
 router.get('/logout', function(req, res) {
-    res.status(200).send({ auth: false, token: null });
+    res.status(200).send({  token: null });
 });
 
-router.get('/me', checarToken, function(req, res, next) {
-
-    User.findById(req.userId, { password: 0 }, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
-
-});
-
-
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
 
 module.exports = router;
 
