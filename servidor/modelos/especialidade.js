d7b755ef94d62af4f2f253d502fcb9c6824b9425
bernardlcs <bernard.lenin4@gmail.com>
2018-06-18 10:25:36 -0300

    commit de mudanças da url e edit do doc txt

---
+++ servidor/modelos/especialidade.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var especi_Schema = new Schema({
+	especialidade:{ type: String, required: true, unique:true}
+
+});
+
+function getNextSequenceValue(sequenceName){
+
+   var sequenceDocument = db.counters.findAndModify({
+      query:{_id: sequenceName },
+      update: {$inc:{sequence_value:1}},
+      new:true
+   });
+	
+   return sequenceDocument.sequence_value;
+}
+
+>db.products.insert({
+   "_id":getNextSequenceValue("productid"),
+   "product_name":"Apple iPhone",
+   "category":"mobiles"
+})
+
+>db.products.insert({
+   "_id":getNextSequenceValue("productid"),
+   "product_name":"Samsung S3",
+   "category":"mobiles"
+})
